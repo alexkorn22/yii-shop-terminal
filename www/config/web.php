bcb4a7ceda558f486a7b570d8d971c0eeef27c9f
Aleksandr <a@test.com>
2017-07-05 22:08:25 +0300

    Init YII. Настройка маршрутизации

--- www/config/web.php
+++ www/config/web.php
@@ -11,6 +11,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'RSv0P-WJ1NtECL77mOcKOA5GTBth5NtV',
+            'baseURL' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -39,14 +40,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
